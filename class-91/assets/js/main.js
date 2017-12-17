ba3fc5bef3e5870e73cb3942d8e65c621b59032d
khondokardidarulislam <33024789+khondokardidarulislam@users.noreply.github.com>
2017-12-18 04:07:56 +0600

    Add files via upload

---
+++ class-91/assets/js/main.js
@@ -0,0 +1,26 @@
+(function($) {
+
+    jQuery(document).ready(function($) {
+    	$(".homepage-slides").owlCarousel({
+    		items: 1,
+    		loop: true,
+    		dots: true,
+    		nav: false,
+    		autoplay: false,
+    	});
+
+        
+
+
+
+
+    });
+
+    jQuery(window).load(function() {
+
+        
+
+
+    });
+
+}(jQuery));
